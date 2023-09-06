7a8823eec54add5dc4d9ac965a7e01e3b27936b1
jeghetermehmet <jeghetermehmet@gmail.com>
2023-09-06 23:31:11 +0200

    Innlevering2

---
+++ Fahreneit_celcius.py
@@ -0,0 +1,7 @@
+f = 16
+print(f)
+c = (f-32)*5/9
+print(c)
+fahrenheit = int(input("Skriv et helltall: "))
+c = (fahrenheit-32)*5/9
+print(c)
---
+++ Utsikriftsfunksjon.py
@@ -0,0 +1,3 @@
+navn = input("Skriv inn navn: ")
+sted = input("Hvor kommer du fra? ")
+print("Hei", navn + "! " + "Du er fra", sted + ".")
